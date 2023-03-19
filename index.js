5199b2fae371b7a76c644691cb12807e5e3bc587
Yisak Girmay <yisakbruck23@gmail.com>
2023-03-19 14:12:40 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 9;
+let num2 = 7;
+let num3 = 9;
+let num4 = 5;
 
-// Write your code here
+let multiply = ( num1 *  num2);
+
+console.log (--multiply);
+
+let random = (Number.isInteger(Math.random()) * 2) + 1;
+
+console.log(random)
+
+let mod = num3 % num4
+
+console.log(mod)
+
+let max = Math.max(1,12,20)
+console.log(max)
+
